25f1dbc0a09611010947cbc95cf870959fe7f254
Holtom <520023290@qq.com>
2022-07-29 00:18:50 +0800

    队列声明

--- src/mq_xxer.erl
+++ src/mq_xxer.erl
@@ -3,7 +3,9 @@
 -include("mq_xxer_common.hrl").
 
 -export([
-    declare_exchange/3
+    declare_exchange/3,
+    declare_queue/2,
+    declare_transient_queue/0
 ]).
 
 %%%===================================================================
@@ -21,12 +23,53 @@ declare_exchange(ExchangeName, ExchangeType, IsDurable) ->
                         type = ExchangeType,
                         durable = IsDurable
                     },
-                    #'exchange.declare_ok'{} = amqp_channel:call(Channel, ExchangeDeclare);
+                    #'exchange.declare_ok'{} = amqp_channel:call(Channel, ExchangeDeclare),
+                    amqp_channel:close(Channel),
+                    ok;
                 _ ->
-                    ?LOG_ERROR("declare_exchange get_connection error~n")
+                    ?LOG_ERROR("declare_exchange get_connection error~n"),
+                    error
             end;
         _ ->
-            ?LOG_ERROR("declare_exchange params error... params:~p~n", [{ExchangeName, ExchangeType, IsDurable}])
+            ?LOG_ERROR("declare_exchange params error... params:~p~n", [{ExchangeName, ExchangeType, IsDurable}]),
+            error
+    end.
+
+%% @doc 声明队列
+declare_queue(QueueName, IsDurable) ->
+    case check_declare_queue_params(QueueName, IsDurable) of
+        true ->
+            case mq_connector:get_connection() of
+                ConnectionPid when is_pid(ConnectionPid) ->
+                    {ok, Channel} = amqp_connection:open_channel(ConnectionPid),
+                    QueueDeclare = #'queue.declare'{
+                        queue = QueueName,
+                        durable = IsDurable
+                    },
+                    #'queue.declare_ok'{} = amqp_channel:call(Channel, QueueDeclare),
+                    amqp_channel:close(Channel),
+                    ok;
+                _ ->
+                    ?LOG_ERROR("declare_queue get_connection error~n"),
+                    error
+            end;
+        _ ->
+            ?LOG_ERROR("declare_queue params error... params:~p~n", [{QueueName, IsDurable}]),
+            error
+    end.
+
+%% @doc 声明临时队列，返回唯一的队列名字
+declare_transient_queue() ->
+    case mq_connector:get_connection() of
+        ConnectionPid when is_pid(ConnectionPid) ->
+            {ok, Channel} = amqp_connection:open_channel(ConnectionPid),
+            QueueDeclare = #'queue.declare'{},
+            #'queue.declare_ok'{queue = QueueName} = amqp_channel:call(Channel, QueueDeclare),
+            amqp_channel:close(Channel),
+            {ok, QueueName};
+        _ ->
+            ?LOG_ERROR("declare_queue get_connection error~n"),
+            error
     end.
 
 %%%===================================================================
@@ -34,3 +77,6 @@ declare_exchange(ExchangeName, ExchangeType, IsDurable) ->
 %%===================================================================
 check_declare_exchange_params(ExchangeName, ExchangeType, IsDurable) ->
     is_binary(ExchangeName) andalso is_binary(ExchangeType) andalso is_boolean(IsDurable).
+
+check_declare_queue_params(QueueName, IsDurable) ->
+    is_binary(QueueName) andalso is_boolean(IsDurable).
